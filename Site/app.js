17262d41c054fc0bb26fb9ce76d7ccb1ace71365
NuloSois <104397117+NuloSois@users.noreply.github.com>
2022-06-24 15:06:20 -0300

    Add files via upload

---
+++ Site/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+let className = document.body.className;
+const switcher = document.querySelector('.btn');
+switcher.textContext = "Dark";
+
+function switching(){
+    document.body.classList.toggle('light-theme'); 
+    document.body.classList.toggle('dark-theme');
+    className = document.body.className;
+    if(className == "light-theme"){switcher.textContent = "Dark";}
+    else{switcher.textContent = "Light";};
+    console.log('current class name: ' + className);};
+    
+
+switcher.addEventListener('click', switching);
